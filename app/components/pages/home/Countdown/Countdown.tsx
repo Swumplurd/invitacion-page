f5453dd46b728ca9fc66f4ba35730376b5eed6b2
swumplurd <53883345+Swumplurd@users.noreply.github.com>
2023-10-27 14:55:25 -0600

    contador texto

--- app/components/pages/home/Countdown/Countdown.tsx
+++ app/components/pages/home/Countdown/Countdown.tsx
@@ -8,20 +8,20 @@ export const Countdown = () => {
   return (
     <div className="flex justify-center gap-8 pb-32 md:gap-11 text-center font-great">
       <div>
-        <h2 className="font-extrabold text-5xl md:text-6xl">{dias}</h2>
-        <h3 className="font-extrabold text-4xl md:text-5xl">Dias</h3>
+        <h2 className="font-extrabold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">{dias}</h2>
+        <h3 className="font-extrabold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">Dias</h3>
       </div>
       <div>
-        <h2 className="font-extrabold text-5xl md:text-6xl">{horas}</h2>
-        <h3 className="font-extrabold text-4xl md:text-5xl">Horas</h3>
+        <h2 className="font-extrabold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">{horas}</h2>
+        <h3 className="font-extrabold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">Horas</h3>
       </div>
       <div>
-        <h2 className="font-extrabold text-5xl md:text-6xl">{minutos}</h2>
-        <h3 className="font-extrabold text-4xl md:text-5xl">Minutos</h3>
+        <h2 className="font-extrabold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">{minutos}</h2>
+        <h3 className="font-extrabold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">Minutos</h3>
       </div>
       <div>
-        <h2 className="font-extrabold text-5xl md:text-6xl">{segundos}</h2>
-        <h3 className="font-extrabold text-4xl md:text-5xl">Segundos</h3>
+        <h2 className="font-extrabold text-3xl sm:text-4xl md:text-5xl lg:text-6xl">{segundos}</h2>
+        <h3 className="font-extrabold text-2xl sm:text-3xl md:text-4xl lg:text-5xl">Segundos</h3>
       </div>
     </div>
   );
