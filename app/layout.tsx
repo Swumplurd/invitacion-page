9eaa079b57c06bfabe0f1f4d1edcf35276e56bf9
swumplurd <53883345+Swumplurd@users.noreply.github.com>
2023-10-27 12:51:28 -0600

    og tags agregadas

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
       <Head>
         <meta property="og:title" content="Invitacion • Ana Fernanda"/>
         <meta property="og:description" content="Invitacion especial para el Baby Shower de Ana Fernanda, por favor entra al link y confirma tu asistencia"/>
-        <meta property="og:image" content="https://img.freepik.com/free-vector/flat-design-bear-family-illustration_23-2149539186.jpg?w=360"/>
+        <meta property="og:image" content="https://img.freepik.com/free-vector/flat-design-bear-family-illustration_23-2149539186.jpg"/>
         <meta property="og:url" content="https://invitacion-ana-fernanda.vercel.app/"/>
       </Head>
       <body className="bg-brown-dark">{children}</body>
